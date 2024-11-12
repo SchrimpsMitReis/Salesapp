39a49e6624c2d58acb82dc8e09df2c46e1872898
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2024-11-12 11:10:30 +0100

    delete all

--- sales/salesapp/models.py
+++ sales/salesapp/models.py
@@ -6,7 +6,7 @@ class Person(models.Model):
     last_name = models.CharField(max_length=30)
     newsletter_abo = models.BooleanField(default=True)
     email_address = models.EmailField(blank=True, null=True, default="")
-    account = models.FloatField(blank=True, null=True, default=None)
+    account = models.FloatField(blank=True, null=True, default=0.0)
 
 
 class Product(models.Model):
